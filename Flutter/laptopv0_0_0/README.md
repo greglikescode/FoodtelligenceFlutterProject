a04950575b2742b8c8dea172adad0f0f00504392
greglikescode <123690402+greglikescode@users.noreply.github.com>
2023-06-26 19:35:59 +0100

    Adding Python Files and LAPTOP flutter files

---
+++ Flutter/laptopv0_0_0/README.md
@@ -0,0 +1,2 @@
+# foodtelligence_main
+ main location of foodtelligence project. Commit changes to actual app here.
---
+++ Flutter/laptopv0_0_0/android/app/src/main/kotlin/com/foodtelligence/foodtelligence/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.foodtelligence.foodtelligence
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
