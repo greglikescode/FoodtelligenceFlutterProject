a3efd6dbe41f73d025c87702d8ec66d519cd78ef
greglikescode <123690402+greglikescode@users.noreply.github.com>
2023-06-29 14:36:40 +0100

    "pcv0_0_0 now has a register system linked to firebase and email "gregorjohnston@icloud.com" has been registered with password "flutterfan4711!". Very happy! picking up momentum now boys!

---
+++ Flutter/pcVersions/pcv0_0_0/lib/views/login_view.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import '../firebase_options.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+}
